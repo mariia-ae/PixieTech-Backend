f3f34e653927261e95ecfaadd10315547df31cb3
MariiaCh21 <chertkovamasha21@icloud.com>
2026-02-19 13:35:35 +0100

    fix css

--- BackendPixieTech/backend/urls.py
+++ BackendPixieTech/backend/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -24,4 +26,5 @@ urlpatterns = [
     path("api/", include("newsletter.urls")),
     path('', include('contact.urls')),
 ]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
